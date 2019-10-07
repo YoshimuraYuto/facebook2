d7e1d3ac205ad9d04e0c55e0066cde13343879f7
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-10-07 10:42:54 +0900

    セッション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :blogs
-  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
