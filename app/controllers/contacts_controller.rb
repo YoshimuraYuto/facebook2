e6171546e7d7a479757002b98351fb852df92834
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-10-07 15:46:48 +0900

    デザイン実装完了

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,23 @@
 class ContactsController < ApplicationController
   def index
   end
+  def new
+    @contact = Contact.new
+  end
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path, notice: "ブログを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+
 end
---
+++ app/models/contact.rb
@@ -0,0 +1,9 @@
+class Contact < ApplicationRecord
+  validates :name, presence: true
+
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
+  validates :content, presence: true
+
+end
